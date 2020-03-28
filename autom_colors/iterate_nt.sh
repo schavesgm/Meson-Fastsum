0309522ca4b55ae09505c8597af35339c7ee4da7
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2020-03-28 19:02:45 +0000

    Now it flushed the data into a new folder to keep things organised

--- autom_colors/iterate_nt.sh
+++ autom_colors/iterate_nt.sh
@@ -2,7 +2,7 @@
 
 channel="g5"
 NT=( 128 64 56 48 40 36 32 28 24 20 16 )
-MESON=( uu ) # us uc ss sc cc )
+MESON=( uu us uc ss sc cc )
 
 mkdir -p conf_test
 ROOT=$( pwd )
@@ -10,7 +10,7 @@ ROOT=$( pwd )
 for meson in ${MESON[@]}; do
     
     echo "I am inside ${meson}"
-    echo "# N_t Cell_ll Plat_ll Cell_ss Plat_ss" > \
+    echo "# N_t Cell_ll Plat_ll Cell_ss Plat_ss Compt" > \
         ./conf_test/conf_${channel}_${meson}.dat
 
     for nt in ${NT[@]}; do
@@ -26,19 +26,17 @@ for meson in ${MESON[@]}; do
         file_fit_ss=$( ls cleanfit_ss* )
 
         # Call the python script
-        res_ll=($( 
-            python ./diff_calc.py ${file_eff_ll} ${file_fit_ll}
+        res=($( 
+            python ./diff_calc.py ${file_eff_ll} ${file_fit_ll} \
+                ${file_eff_ss} ${file_eff_ss}
         ))
-        res_ss=($( 
-            python ./diff_calc.py ${file_eff_ss} ${file_fit_ss}
-        ))
-        ll_part="${res_ll[0]} ${res_ll[1]}"
-        ss_part="${res_ss[0]} ${res_ss[1]}"
-        echo "${nt} ${ll_part} ${ss_part}" >> \
+        ll_part="${res[0]} ${res[1]}"
+        ss_part="${res[2]} ${res[3]}"
+
+        echo "${nt} ${ll_part} ${ss_part} ${res[4]}" >> \
             ${ROOT}/conf_test/conf_${channel}_${meson}.dat
 
         rm ./diff_calc.py
         cd ../../
-        
     done
 done
